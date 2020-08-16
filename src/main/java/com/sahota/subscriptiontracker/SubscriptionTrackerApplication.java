cf9bcf55bfaa24f5eac0640c67de3e49c0bd18ba
Amrit Sahota <amritsahota@Amrits-MBP.fios-router.home>
2020-08-16 19:47:02 -0400

    first commit

---
+++ src/main/java/com/sahota/subscriptiontracker/SubscriptionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sahota.subscriptiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionTrackerApplication.class, args);
+	}
+
+}
