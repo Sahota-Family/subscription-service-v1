bb64b6dbc94f9e90f473bdf0eaeeaabf6a8cfe4a
Harman Sahota <harman.sahota93@gmail.com>
2020-09-03 01:22:40 -0400

    Added DB config; converted to yml

---
+++ src/main/java/com/sahota/subscriptiontracker/config/DataSourceConfig.java
@@ -0,0 +1,33 @@
+package com.sahota.subscriptiontracker.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+
+        return dataSourceBuilder.build();
+    }
+}
